70444696093384b95294ea5e2b2998f07c1e482d
Akhil-28407 <ibascakhil@gmail.com>
2025-11-30 04:26:14 +0530

    dotenv added to gitignore

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
+dotenv.config();
 const app = express();
-
 // middleware
 app.use(cors());
 app.use(express.json());
-
 // routes
 app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
+app.use("/api/projects", projectRoutes)
 app.use("/api/users", userRoutes);
-
-// MongoDB connection (no password)
-const MONGO_URI = "mongodb://127.0.0.1:27017/student_portfolio";
+// MongoDB connection using Environment Variable
+const MONGO_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(MONGO_URI)
   .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log("Server running on port", PORT));
+    console.log("✅ Connected to MongoDB Atlas");
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running on port ${PORT}`)
+    );
   })
-  .catch((err) => console.error("Mongo error", err));
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
